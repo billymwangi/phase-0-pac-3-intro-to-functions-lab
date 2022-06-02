64afead9708065fdaf526d10389c4b22428b0cc0
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-02 11:36:40 +0300

    Done.

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ function logWhisper(string) {
     console.log (string.toLowerCase());
 }
 function sayHiToHeadphonedRoommate(string) {
-    if string = return `I can\`t hear you!` 
-}
+    if (string= string.toLowerCase)  {
+        return "I can`t hear you!";
+
+}   if (string = string.toUpperCase) {
+        return "YES INDEED!";
+        
+}   if (string = "Let`s have dinner together!" ) {
+        return "I would love to!"; }
